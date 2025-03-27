5b8215b857e79535aac6b4100109e07a953e851f
CRISTIAN CIUREA <cristianciurea@gmail.com>
2025-03-27 13:28:33 +0200

    update

---
+++ Seminar_5/Sem5PAW_1063/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace Sem5PAW_1063
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbEUR = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbUSD = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbGBP = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbXAU = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbData
+            // 
+            this.tbData.Location = new System.Drawing.Point(125, 46);
+            this.tbData.Name = "tbData";
+            this.tbData.Size = new System.Drawing.Size(100, 22);
+            this.tbData.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data curs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Curs EUR";
+            // 
+            // tbEUR
+            // 
+            this.tbEUR.Location = new System.Drawing.Point(125, 106);
+            this.tbEUR.Name = "tbEUR";
+            this.tbEUR.Size = new System.Drawing.Size(100, 22);
+            this.tbEUR.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Curs USD";
+            // 
+            // tbUSD
+            // 
+            this.tbUSD.Location = new System.Drawing.Point(125, 167);
+            this.tbUSD.Name = "tbUSD";
+            this.tbUSD.Size = new System.Drawing.Size(100, 22);
+            this.tbUSD.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Curs GBP";
+            // 
+            // tbGBP
+            // 
+            this.tbGBP.Location = new System.Drawing.Point(125, 231);
+            this.tbGBP.Name = "tbGBP";
+            this.tbGBP.Size = new System.Drawing.Size(100, 22);
+            this.tbGBP.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 305);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Curs XAU";
+            // 
+            // tbXAU
+            // 
+            this.tbXAU.Location = new System.Drawing.Point(125, 302);
+            this.tbXAU.Name = "tbXAU";
+            this.tbXAU.Size = new System.Drawing.Size(100, 22);
+            this.tbXAU.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 40);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Parsare";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(281, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 40);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Generare";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(403, 49);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(224, 142);
+            this.treeView1.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(403, 228);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 36);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Populare";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbXAU);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbGBP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbUSD);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbEUR);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbEUR;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbUSD;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbGBP;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbXAU;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Seminar_5/Sem5PAW_1064/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Sem5PAW_1064
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbData = new System.Windows.Forms.TextBox();
+            this.tbEUR = new System.Windows.Forms.TextBox();
+            this.tbUSD = new System.Windows.Forms.TextBox();
+            this.tbGBP = new System.Windows.Forms.TextBox();
+            this.tbXAU = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data curs";
+            // 
+            // tbData
+            // 
+            this.tbData.Location = new System.Drawing.Point(142, 43);
+            this.tbData.Name = "tbData";
+            this.tbData.Size = new System.Drawing.Size(100, 22);
+            this.tbData.TabIndex = 1;
+            // 
+            // tbEUR
+            // 
+            this.tbEUR.Location = new System.Drawing.Point(142, 91);
+            this.tbEUR.Name = "tbEUR";
+            this.tbEUR.Size = new System.Drawing.Size(100, 22);
+            this.tbEUR.TabIndex = 2;
+            // 
+            // tbUSD
+            // 
+            this.tbUSD.Location = new System.Drawing.Point(142, 143);
+            this.tbUSD.Name = "tbUSD";
+            this.tbUSD.Size = new System.Drawing.Size(100, 22);
+            this.tbUSD.TabIndex = 3;
+            // 
+            // tbGBP
+            // 
+            this.tbGBP.Location = new System.Drawing.Point(142, 195);
+            this.tbGBP.Name = "tbGBP";
+            this.tbGBP.Size = new System.Drawing.Size(100, 22);
+            this.tbGBP.TabIndex = 4;
+            // 
+            // tbXAU
+            // 
+            this.tbXAU.Location = new System.Drawing.Point(142, 250);
+            this.tbXAU.Name = "tbXAU";
+            this.tbXAU.Size = new System.Drawing.Size(100, 22);
+            this.tbXAU.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Curs EUR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Curs USD";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Curs GBP";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(62, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Curs XAU";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 41);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Parsare";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 41);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Generare";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbXAU);
+            this.Controls.Add(this.tbGBP);
+            this.Controls.Add(this.tbUSD);
+            this.Controls.Add(this.tbEUR);
+            this.Controls.Add(this.tbData);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbData;
+        private System.Windows.Forms.TextBox tbEUR;
+        private System.Windows.Forms.TextBox tbUSD;
+        private System.Windows.Forms.TextBox tbGBP;
+        private System.Windows.Forms.TextBox tbXAU;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
